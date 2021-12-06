ee2c1ba0a9e2de72b166e38a9f0977665a4ac034
Mike Mora <reg.mike.mora@gmail.com>
2021-12-05 22:06:45 -0500

    Challenge4 - Add public path for assets

--- myApp.js
+++ myApp.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+var path = require('path')
 var PORT = 3100
-var homeRoute = __dirname + `/views/index.html`
+var homeRoute = path.join(__dirname, `views/index.html`)
+var assetsRoute = path.join(__dirname, `public`)
+
+app.use('/public', express.static(assetsRoute))
 
 app.get('/', (req, res) => {
   res.sendFile(homeRoute)
