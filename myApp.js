55a1b1f437680fec72805eee2f003d2c88210259
Mike Mora <reg.mike.mora@gmail.com>
2021-12-06 08:47:06 -0500

    Challenge6 - Add logger to root path

--- myApp.js
+++ myApp.js
@@ -6,6 +6,11 @@ var PORT = 3100
 var homeRoute = path.join(__dirname, `views/index.html`)
 var assetsRoute = path.join(__dirname, `public`)
 
+app.use('/', (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+
 app.use('/public', express.static(assetsRoute))
 
 app.get('/', (req, res) => {
