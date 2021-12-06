e4de7a6503f91bf60e78fa2ccc001c6ca7a90e46
Mike Mora <reg.mike.mora@gmail.com>
2021-12-05 21:50:47 -0500

    Challenge3 - return html file

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 var PORT = 3100
+var homeRoute = __dirname + `/views/index.html`
 
 app.get('/', (req, res) => {
-  res.send("Hello Express")
+  res.sendFile(homeRoute)
 })
 
 app.listen(PORT, () => {
